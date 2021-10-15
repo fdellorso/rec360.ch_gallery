fb004ceb5cc2d5b866538eeb350d3b61eaa0e9c5
Francesco Dell'Orso <francesco.dellorso@gmail.com>
2021-10-15 16:43:28 +0200

    update

--- js/gallery/createcard.js
+++ js/gallery/createcard.js
@@ -4,8 +4,22 @@
 import { createElement } from '../utility/createelement.js'
 
 const galleryDemo = [
-  { gallery_id: 1, name_value: '1', media_value: 'photo', quantity: 7, date_value: '06-10-2021', time_value: '12:00:00', new: true, published: false },
-  { gallery_id: 2, name_value: 'ipod', media_value: 'photo360', quantity: 72, date_value: '06-10-2021', time_value: '12:00:00', new: true, published: false }
+  { gallery_id: 1, name_value: 'PHOTO_20211013_122248', media_value: 'photo360', quantity: 36, date_value: '06-10-2021', time_value: '12:00:00', new: true, published: false },
+  { gallery_id: 2, name_value: 'PHOTO_20211013_122248_NOBG', media_value: 'photo360', quantity: 36, date_value: '06-10-2021', time_value: '12:00:00', new: true, published: false },
+  { gallery_id: 3, name_value: 'PHOTO_20211013_132652', media_value: 'photo360', quantity: 36, date_value: '06-10-2021', time_value: '12:00:00', new: true, published: false },
+  { gallery_id: 4, name_value: 'PHOTO_20211013_132652_NOBG', media_value: 'photo360', quantity: 36, date_value: '06-10-2021', time_value: '12:00:00', new: true, published: false },
+  { gallery_id: 5, name_value: 'PHOTO_20211013_134205', media_value: 'photo360', quantity: 36, date_value: '06-10-2021', time_value: '12:00:00', new: true, published: false },
+  { gallery_id: 6, name_value: 'PHOTO_20211013_134205_NOBG', media_value: 'photo360', quantity: 36, date_value: '06-10-2021', time_value: '12:00:00', new: true, published: false },
+  { gallery_id: 7, name_value: 'PHOTO_20211013_134812', media_value: 'photo360', quantity: 36, date_value: '06-10-2021', time_value: '12:00:00', new: true, published: false },
+  { gallery_id: 8, name_value: 'PHOTO_20211013_134812_NOBG', media_value: 'photo360', quantity: 36, date_value: '06-10-2021', time_value: '12:00:00', new: true, published: false },
+  { gallery_id: 9, name_value: 'PHOTO_20211013_140217', media_value: 'photo360', quantity: 36, date_value: '06-10-2021', time_value: '12:00:00', new: true, published: false },
+  { gallery_id: 10, name_value: 'PHOTO_20211013_140217_NOBG', media_value: 'photo360', quantity: 36, date_value: '06-10-2021', time_value: '12:00:00', new: true, published: false },
+  { gallery_id: 11, name_value: 'PHOTO_20211013_141230', media_value: 'photo360', quantity: 36, date_value: '06-10-2021', time_value: '12:00:00', new: true, published: false },
+  { gallery_id: 12, name_value: 'PHOTO_20211013_141230_NOBG', media_value: 'photo360', quantity: 36, date_value: '06-10-2021', time_value: '12:00:00', new: true, published: false },
+  { gallery_id: 13, name_value: 'PHOTO_20211013_142937', media_value: 'photo360', quantity: 36, date_value: '06-10-2021', time_value: '12:00:00', new: true, published: false },
+  { gallery_id: 14, name_value: 'PHOTO_20211013_142937_NOBG', media_value: 'photo360', quantity: 36, date_value: '06-10-2021', time_value: '12:00:00', new: true, published: false },
+  { gallery_id: 15, name_value: 'PHOTO_20211013_143947', media_value: 'photo360', quantity: 36, date_value: '06-10-2021', time_value: '12:00:00', new: true, published: false },
+  { gallery_id: 16, name_value: 'PHOTO_20211013_143947_NOBG', media_value: 'photo360', quantity: 36, date_value: '06-10-2021', time_value: '12:00:00', new: true, published: false }
 ]
 
 // async function getListGallery () {
